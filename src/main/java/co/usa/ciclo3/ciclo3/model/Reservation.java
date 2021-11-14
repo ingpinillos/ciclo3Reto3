cebe6840c0a428734428ea70f905710a4c0df466
Javier Pinillos <ingpinillos@yahoo.com>
2021-11-14 00:38:17 -0500

    2021.11.14 00.37

--- src/main/java/co/usa/ciclo3/ciclo3/model/Reservation.java
+++ src/main/java/co/usa/ciclo3/ciclo3/model/Reservation.java
@@ -32,7 +32,7 @@ public class Reservation implements Serializable {
 
     @ManyToOne
     @JoinColumn(name = "tool")
-    @JsonIgnoreProperties({"reservations", "messages"})
+    @JsonIgnoreProperties({"reservations"})
     private Tool tool;
     
     @ManyToOne
